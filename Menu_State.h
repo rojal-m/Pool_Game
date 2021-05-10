99e5dd7146ded7ccc630d4bc63251aed325bb642
angar174 <angar174@tlvm-5-4-1.ad.liu.se>
2021-05-10 11:55:18 +0200

    Menu_State

--- Menu_State.h
+++ Menu_State.h
@@ -1,1 +1,27 @@
-//1111
+
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include "State.h"
+
+
+class Menu_State: public State
+{
+public:
+  Menu_State();
+  virtual bool vs_bot();
+  virtual bool vs_player();
+  virtual void handle_event(sf::Event e) override;
+  virtual void update() override;
+  virtual void render(sf::RenderTarget & t) override;
+  virtual int get_next_state() override;
+
+
+private:
+  sf::Text text;
+  sf::Font font{};
+  bool play;
+  Player player_1;
+  Player player_2;
+
+};
