2518fbd789d83cba59711f42903758887b07c7e1
angar174 <angar174@tlvm-4-4-1.ad.liu.se>
2021-05-17 09:48:07 +0200

    Game.h

--- Game.h
+++ Game.h
@@ -2,95 +2,28 @@
 #define GAME_H
 
 #include <SFML/Graphics.hpp>
-
-#include <string>
 #include <memory>
+#include <string>
 
-//#include "State.h"
-
-class Game {
-
+class Game
+{
 public:
 
-    /*
-     * Constructor of Game.
-     *
-     * Will perform these tasks:
-     * - initialize the current_state with an initial state (welcome
-     *   screen),
-     * - Fill the states map with the States needed during the game
-     * - spawn a window with the specified titlebar, width and height
-     *   (in pixels)
-     */
-    Game (std::string const & title, unsigned width, unsigned height);
-
-    /*
-     * Run the main loop of the state machine, and therefore by extension
-     * the entire game.
-     *
-     * Each iteration of the main loop go through these steps:
-     * - Handle all window events from the event queue (these include
-     *   key presses/releases, mouse movement, resizing of the window
-     *   and more).
-     *
-     *   What this means is that we check if the user has requested
-     *   to close the window and therefore the game, and for any other
-     *   events we pass them on to the state.
-     * - Update the current state.
-     *
-     *   This means running any logic that the state might have, for
-     *   example; move objects, check for collision, manage resources
-     *   etc. This step will probably perform the vast majority of the
-     *   logic in the project (depending on the project of course).
-     *
-     * - Draw the currently active state to the window.
-     *
-     *   This step will simply pass the window to the state and will
-     *   thus allow the state to draw itself onto the windows. This
-     *   is beneficial since we now have completely decoupled the
-     *   state from the Game class, thus allowing all behaviour of a
-     *   state to be implemented in the corresponding class without
-     *   having to touch any other code.
-     */
-    void start ();
+Game(std::string const & titel, unsigned width, unsigned screen_height);
+void start();
 
 private:
-    /*
-     * SFML representation of window which we can draw on.
-    */
-    sf::RenderWindow window;
 
+  std::map<int, std::unique_ptr<State>> States;
+  int current_state;
+  sf::RenderWindow window;
+  bool running;
 
-    /*
-     * Container for all states in the game.
-     * Current_state keeps track on the current state
-     */
-    std::map<int, std::unique_ptr<State>> states;
-    int current_state;
+  enum ball_position {};
+  enum ball_name{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
 
-
-    /*
-     * If true, then the game should keep on run, otherwise the next
-     * iteration of the game loop will not run.
-     */
-    bool running;
-
-
-    void handle_events();
-
-    /*
-     * This function puts the program to sleep for a certain
-     * period of time in order to keep a steady framerate.
-     * The inargument is how many milliseconds the current iteration
-     * took to complete.
-     *
-     * Say that the game is supposed to run at 60 frames per second (fps).
-     * This means that each frame should take exactly 1000/60 = 16.666...
-     * milliseconds to run. If it took longer, then there is not much
-     * we can do about it, but if it took a shorter amount of time,
-     * then request that our program sleep for the remaining time.
-     */
-    void delay (sf::Clock & clock) const;
+  void hnandle_events();
+  void delay(sf::Clock & c) const;
 };
 
-#endif // GAME_H
+#endif;
