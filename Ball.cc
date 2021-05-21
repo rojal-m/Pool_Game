9c1b4276ff13c7d92f716626fa341a677c341877
angar174 <angar174@tlvm-4-1-1.ad.liu.se>
2021-05-21 23:01:22 +0200

    cc filen uppdaterad med de nya funktionerna

--- Ball.cc
+++ Ball.cc
@@ -6,24 +6,33 @@
 
 using namespace sf;
 
-Ball::Ball(Texture& t)
-  :ball{t}, position{}, velocity{0,0}
+Ball::Ball(Vector2f const& Pos, Item & I, Col c)
+:ball{I.getBallTextureByColor(c)}, position{Pos}, velocity{0,0}, moving{false}, color{c}, visible{true}
 {
   auto a = ball.getGlobalBounds();
   ball.setOrigin(a.width/2,a.height/2);
-  position = Vector2f{413.0,413.0};
 }
 
 void Ball::update()
 {
-  position.x += velocity.x/delta;
-  position.y += velocity.y/delta;
-  velocity.x =  velocity.x*0.98;
-  velocity.y =  velocity.y*0.98;
+  if(!visible)
+  return;
+
+ position += velocity/delta;
+ velocity =  velocity*FRICTION;
+
+ if(length(velocity) < 10)
+ {
+   velocity.x = 0;
+   velocity.y = 0;
+   moving = false;
+  }
 }
 
 void Ball::draw(RenderWindow& w)
 {
+  if(!visible)
+  return;
   ball.setPosition(position);
   w.draw(ball);
 }
@@ -32,75 +41,95 @@ void Ball::onShoot(float&p,float r)
 {
   velocity.x = p * cos((r*pi)/180);
   velocity.y = p * sin((r*pi)/180);
-
+  moving = true;
 }
 
-Stick::Stick(Texture& t,Ball & b)
-  :stick{t}, ball_position{}, rotation {0}, Wball{b}, power{0}
+void Ball::handleBallInHole(Hole const& h)
 {
-  stick.setOrigin(970,11);
-  position = Vector2f{413.0,413.0};
+  if(!visible)
+  return;
+  bool inHole{};
+  Vector2f pos{position};
+  std::for_each(h.position.begin(),h.position.end(),[&inHole,&h,&pos](auto const& p){
+  inHole += distFrom(pos,p) <= h.radius;
+  });
+  if(!inHole)
+{
+  return;
+}
+  visible = false;
+  moving = false;
 }
 
-void Stick::update(Event::MouseMoveEvent & v)
+void Ball::collideWith(Ball & b)
 {
-  updateRotation(v);
+  if(!visible || !B.visible)
+  return;
+  //find normal vector
+  const auto n{position - B.position};
+
+  //find distance
+  const auto dist{length(n)};
+  if(dist > BALL_DIAMETER)
+  return;
+  const auto mtd{n*((BALL_DIAMETER-dist)/dist)};
+  position = position + mtd*(0.5f);
+  B.position = B.position - mtd*(0.5f);
+
+  //find unit normal vector
+  const auto un{n*(1/length(n))};
+  //find unit tangent vector
+  const auto ut{Vector2f{-un.y,un.x}};
+  //Project velocity onto the unit normal and unit tangent vectors.
+  const auto v1n{dot(un,velocity)};
+  const auto v1t{dot(ut,velocity)};
+  const auto v2n{dot(un,B.velocity)};
+  const auto v2t{dot(ut,B.velocity)};
+
+
+  const auto v1nTag{un*v2n};
+  const auto v1tTag{ut*v1t};
+  const auto v2nTag{un*v1n};
+  const auto v2tTag{ut*v2t};
+
+  velocity = v1nTag + v1tTag;
+  B.velocity = v2nTag + v2tTag;
+
+  moving = true;
+  B.moving = true;
 }
 
-void Stick::draw(RenderWindow& w)
+void Ball::collideWith2(Wall & w)
 {
-  stick.setRotation(rotation);
-  stick.setPosition(position);
-  w.draw(stick);
+if(!visible || !moving)
+return;
+
+bool collided = false;
+if(position.y <= w.topY+(BALL_DIAMETER/2))
+{
+  position.y = w.topY+(BALL_DIAMETER/2);
+  velocity = Vector2f{velocity.x,-velocity.y};
+  collided = true;
+}
+if(position.x >= w.rightX-(BALL_DIAMETER/2))
+{
+  position.x = w.rightX-(BALL_DIAMETER/2);
+  velocity = Vector2f{-velocity.x,velocity.y};
+  collided = true;
+}
+if(position.y >= w.bottomY-(BALL_DIAMETER/2))
+{
+  position.y = w.bottomY-(BALL_DIAMETER/2);
+  velocity = Vector2f{velocity.x,-velocity.y};
+  collided = true;
+}
+if(position.x <= w.leftX+(BALL_DIAMETER/2))
+{
+  position.x = w.leftX+(BALL_DIAMETER/2);
+  velocity = Vector2f{-velocity.x,velocity.y};
+  collided = true;
 }
 
-void Stick::updateRotation(Event::MouseMoveEvent & v)
-{
-  float opposite = v.y - position.y;
-  float adjescent = v.x - position.x;
-  if(opposite < 0 && adjescent < 0)
-    rotation = 180+(180*atan(opposite/adjescent))/pi;
-  if(opposite > 0 && adjescent < 0)
-    rotation = 180+(180*atan(opposite/adjescent))/pi;
-  if(opposite < 0 && adjescent > 0)
-    rotation = 360+(180*atan(opposite/adjescent))/pi;
-   if(opposite > 0 && adjescent > 0)
-    rotation = (180*atan(opposite/adjescent))/pi;
-}
-
-void Stick::increasePower()
-{
-  power += 100;
-  Vector2f v = stick.getOrigin();
-  stick.setOrigin(v.x+5,v.y);
-}
-
-void Stick::shoot()
-{
-  Wball.onShoot(power,rotation);
-  power = 0;
-  stick.setOrigin(950,11);
-}
-
-Hole::Hole()
-:ball_position{};
-
-bool Hole::is_in(Ball& b)
-{
-  if (b== 1)
-{
-  return true;
-}
-else
-{
-  return false;
-}
-}
-
-void Hole::if_in(bool x, int p)
-{
-  if (x==true)
-  {
-    points++;
-  }
+if(collided)
+  velocity *= FRICTION;
 }
