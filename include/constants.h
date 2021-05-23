7ec2ce50e84863ed0e1ed061e2d8bec4911fed35
angar174 <angar174@tlvm-4-3-4.ad.liu.se>
2021-05-23 13:48:28 +0200

    fricition and delta changes

--- include/constants.h
+++ include/constants.h
@@ -3,10 +3,10 @@
 int const screen_width  { 1500 };
 int const screen_height { 825 };
 float const pi { 3.14159265359 };
-float const delta { 1000 };
+float const delta { 100 };
 float const BALL_DIAMETER { 36 };
 float const MAX_POWER { 5000 };
-float const FRICTION { 0.9962330 };
+float const FRICTION { 0.98 };
 double const fps { 120.0 };
 
 enum Col
