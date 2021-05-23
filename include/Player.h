fc817f7dfef5dd5eb05add53dd4ecffbeedf37b3
rityo409 <rityo409@tlvm-4-1-4.ad.liu.se>
2021-05-23 23:02:36 +0200

    Player.h klassen för 2 spelare, en start

--- include/Player.h
+++ include/Player.h
@@ -1,28 +1,24 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#include <string>
+#include <SFML/Graphics.hpp>
+
 class Player
 {
-
-
 public:
+  Player(Col, std::string);
 
-  Player();
+  bool takeTurn();
+  void shootBall();
 
-
-
+  int playerID;
+  bool turn;
 
 private:
-
-
-
-
-
-
-
-
-
-
+  Col assignBalls;
+  std::string playerName;
+  Ball* currBall;
 };
 
 
