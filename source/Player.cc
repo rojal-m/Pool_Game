84f27dc719b960bc385a2d516eea48e30ad0931b
rityo409 <rityo409@tlvm-4-1-4.ad.liu.se>
2021-05-23 23:03:39 +0200

    cc fil för player.h

---
+++ source/Player.cc
@@ -0,0 +1,19 @@
+#include "Player.h"
+#include "Stick.h"
+
+using namespace std;
+using namespace sf;
+
+Player::Player(Col B, string N)
+:assignBalls{B}, playerName{N
+{}
+
+bool Player::takeTurn()
+{
+
+}
+
+void Player::shootBall()
+{
+
+}
