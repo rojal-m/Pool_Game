2533f70b0fd22d6f860f6d17a9b45e3249a23479
angar174 <angar174@tlvm-4-3-4.ad.liu.se>
2021-05-23 13:29:22 +0200

    Fixed

--- include/Item.h
+++ include/Item.h
@@ -2,23 +2,32 @@
 #define ITEM_H
 
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include <string>
 #include "constants.h"
+
+using namespace sf;
 
 class Item
 {
 public:
-  sf::Texture Background;
-  sf::Texture Stick;
-  sf::Texture W_B;
-  sf::Texture B_B;
-  sf::Texture B1;
-  sf::Texture B2;
-  sf::Texture GameOver;
+  Texture Background;
+  Texture Stick;
+  Texture W_B;
+  Texture B_B;
+  Texture B1;
+  Texture B2;
+  Texture GameOver;
+
+  SoundBuffer collideBuffer;
+  SoundBuffer holeBuffer;
+  SoundBuffer strikeBuffer;
+  SoundBuffer sideBuffer;
 
   Item();
-  sf::Texture loadTexture(std::string const& s);
-  sf::Texture const& getBallTextureByColor(Col C) const;
+  Texture loadTexture(std::string const& s);
+  SoundBuffer loadSoundBuffer (std::string const& a);
+  Texture const& getBallTextureByColor(Col C);
 };
 
 #endif
